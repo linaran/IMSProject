63381fdae7796e1356ed2134063698a59da7f5db
Dario Smolcic <dario.smolcic@fer.hr>
2016-06-15 23:38:32 +0200

    Ghost AI
    Ghosts still don't know how to avoid pacman when he is on powerup.
    myInfo.hasProperty(PacmanAgent.powerupPropertyName) doesnt work.
    We need to find a way to read that property if it is possible.

--- src/reimplement/GhostAI.java
+++ src/reimplement/GhostAI.java
@@ -1,77 +1,208 @@
 package reimplement;
 
-import mmaracic.gameaiframework.AgentAI;
-import mmaracic.gameaiframework.PacmanVisibleWorld;
-import mmaracic.gameaiframework.WorldEntity;
+import mmaracic.gameaiframework.*;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Random;
+import java.util.*;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.max;
 
 /**
  *
  */
 public class GhostAI extends AgentAI {
-  private Date now = new Date();
-  private Random r = new Random(now.getTime());
+  protected static class Location implements Comparable<Location> {
+    int x = 0, y = 0;
 
-  private int findClosest(ArrayList<int[]> moves, int[] pos) {
-    int index = 0;
-    int[] move = moves.get(0);
-    float dist = Math.abs(pos[0] - move[0]) + Math.abs(pos[1] - move[1]);
-    for (int i = 1; i < moves.size(); i++) {
-      move = moves.get(i);
-      float currDist = Math.abs(pos[0] - move[0]) + Math.abs(pos[1] - move[1]);
-      if (currDist < dist) {
-        dist = currDist;
-        index = i;
+    Location(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    int getX() {
+      return x;
+    }
+
+    int getY() {
+      return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (o instanceof Location) {
+        Location temp = (Location) o;
+        return (temp.x == this.x) && (temp.y == this.y);
+      } else
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+      int hash = 7;
+      hash = 79 * hash + this.x;
+      hash = 79 * hash + this.y;
+      return hash;
+    }
+
+    public float distanceTo(Location other) {
+      int distanceX = other.x - x;
+      int distanceY = other.y - y;
+
+      return (float) abs(distanceX) + abs(distanceY);
+//            return (float) Math.sqrt(distanceX*distanceX + distanceY+distanceY);
+    }
+
+    @Override
+    public int compareTo(Location o) {
+      if (x == o.x) {
+        return Integer.compare(y, o.y);
+      } else {
+        return Integer.compare(x, o.x);
       }
     }
-    return index;
   }
 
-  @Override
-  public int decideMove(ArrayList<int[]> moves, PacmanVisibleWorld mySurroundings, WorldEntity.WorldEntityInfo myInfo) {
-    int[] move = null;
+  private Location myLocation = new Location(0, 0);
 
-    for (int i = -mySurroundings.getDimensionX() / 2; i <= mySurroundings.getDimensionX() / 2; i++) {
-      for (int j = -mySurroundings.getDimensionY() / 2; j <= mySurroundings.getDimensionY() / 2; j++) {
-        if (i == 0 && j == 0) continue;
-        //find pacman
-        ArrayList<WorldEntity.WorldEntityInfo> elements = mySurroundings.getWorldInfoAt(i, j);
-        HashMap<Integer, Object> metaHash = mySurroundings.getWorldMetadataAt(i, j);
-        if (elements != null && metaHash != null) {
-          for (WorldEntity.WorldEntityInfo el : elements) {
-            if (el.getIdentifier().compareToIgnoreCase("Pacman") == 0) {
-              int index = findClosest(moves, new int[]{i, j});
-              metaHash.clear();
-              metaHash.put(myInfo.getID(), moves.get(index));
-              return index;
-            }
-          }
-          //Check if someone else found him
-          if (!metaHash.isEmpty()) {
-            for (Integer id : metaHash.keySet()) {
-              if (id != myInfo.getID()) {
-                move = (int[]) metaHash.remove(id);
-                //printStatus(myInfo.getID()+": Found pacman trail left by ghost: "+id+"!");
+  private float[] w = {500, 10};
+
+  private static Location pacmanLocation = null;
+  private static final int BUFFER_SIZE = 1000;
+  private static ArrayList<Location> lastLocations = new ArrayList<Location>(BUFFER_SIZE);
+  private static int index = 0;
+
+  private float getDistance(PacmanVisibleWorld mySurroundings, Location location, String element) {
+    int radiusX = mySurroundings.getDimensionX() / 2;
+    int radiusY = mySurroundings.getDimensionY() / 2;
+
+    float distance = Float.MAX_VALUE;
+    for (int i = -radiusX; i <= radiusX; i++) {
+      for (int j = -radiusY; j <= radiusY; j++) {
+        if (i==0 && j==0) continue;
+        Location tempLocation = new Location(i, j);
+        if (abs(tempLocation.getX()) > radiusX || abs(tempLocation.getY()) > radiusY ) continue;
+        ArrayList<WorldEntity.WorldEntityInfo> neighPosInfos = mySurroundings.getWorldInfoAt(tempLocation.getX(), tempLocation.getY());
+        if (neighPosInfos != null) {
+          for (WorldEntity.WorldEntityInfo info : neighPosInfos) {
+            if (info.getIdentifier().compareToIgnoreCase(element) == 0) {
+              //Remember him!
+
+              float currDistance = location.distanceTo(tempLocation);
+              if (element == "Pacman") pacmanLocation = tempLocation;
+              if (currDistance < distance) {
+                distance = currDistance;
               }
             }
           }
         }
       }
     }
-    //Go where metadata pointed
-    if (move != null) {
-      for (int i = 0; i < moves.size(); i++) {
-        int[] m = moves.get(i);
-        if (m[0] == move[0] && m[1] == move[1]) {
-          return i;
+    return distance;
+  }
+
+  private float isLastLocation(Location loc) {
+
+    if (lastLocations.size() == 0)
+      return 1;
+    int counter = BUFFER_SIZE;
+    //System.out.println(loc.equals(lastLocation));
+    for (int i=index-1; i >=0; i--) {
+      if (lastLocations.get(i).equals(loc)) {
+        int occurrences = Collections.frequency(lastLocations, lastLocations.get(i));
+        return -1.0f * occurrences * counter / BUFFER_SIZE;
+      }
+      counter++;
+    }
+
+    if (lastLocations.size()<BUFFER_SIZE)
+      return 1;
+
+    for (int i=BUFFER_SIZE-1; i >=index; i--) {
+      if (lastLocations.get(i).equals(loc)) {
+        int occurrences = Collections.frequency(lastLocations, lastLocations.get(i));
+        return -1.0f * occurrences * counter / BUFFER_SIZE;
+      }
+      counter++;
+    }
+    return 1;
+  }
+
+  private float qScore(PacmanVisibleWorld mySurroundings, Location loc, WorldEntity.WorldEntityInfo myInfo) {
+    ArrayList<WorldEntity.WorldEntityInfo> neighPosInfos = mySurroundings.getWorldInfoAt(loc.getX(), loc.getY());
+    if (neighPosInfos != null) {
+      for (WorldEntity.WorldEntityInfo info : neighPosInfos) {
+        if (info.getIdentifier().compareToIgnoreCase("Wall") == 0) {
+          return -Float.MAX_VALUE;
         }
       }
     }
-    //Go random
-    return r.nextInt(moves.size());
+
+    float pacmanDistance = getDistance(mySurroundings, loc, "Pacman");
+    if (pacmanDistance > 10 && pacmanLocation != null) {
+      pacmanDistance = loc.distanceTo(pacmanLocation);
+    }
+    Location location = new Location(myLocation.getX() + loc.getX(), myLocation.getY() + loc.getY());
+    boolean powerUP = myInfo.hasProperty(PacmanAgent.powerupPropertyName);
+    int reverse = 1;
+    if (powerUP) reverse = -1;
+    float q = reverse * w[0] / (pacmanDistance + 0.1f) + w[1] * isLastLocation(location) ;
+    return q;
+  }
+
+  private void addLocation(Location loc) {
+    if (lastLocations.size() < BUFFER_SIZE) {
+      lastLocations.add(loc);
+    }
+    else {
+      lastLocations.add(index, myLocation);
+    }
+    index = (index + 1) % BUFFER_SIZE;
+  }
+
+  private float bestActionScore(PacmanVisibleWorld mySurroundings, Location loc, WorldEntity.WorldEntityInfo myInfo) {
+    float leftScore = qScore(mySurroundings, new Location(loc.getX()-1, loc.getY()), myInfo);
+    float rightScore = qScore(mySurroundings, new Location(loc.getX()+1, loc.getY()), myInfo);
+    float upScore = qScore(mySurroundings, new Location(loc.getX(), loc.getY()+1), myInfo);
+    float downScore = qScore(mySurroundings, new Location(loc.getX(), loc.getY()-1), myInfo);
+    return max(max(leftScore, rightScore), max(upScore, downScore));
+  }
+
+  @Override
+  public int decideMove(ArrayList<int[]> moves, PacmanVisibleWorld mySurroundings, WorldEntity.WorldEntityInfo myInfo) {
+    //System.out.println("Pocetak");
+
+    float q = qScore(mySurroundings, new Location(0, 0), myInfo);
+
+    Location nextLocation = myLocation;
+    int moveIndex = 0;
+    Random rand = new Random();
+
+    float gamma = 0.7f;
+    float maxScore = -Float.MAX_VALUE;
+    for (int i = moves.size() - 1; i >= 0; i--) {
+      int[] move = moves.get(i);
+      //System.out.printf("%d %d\n", move[0], move[1]);
+      Location moveLocation = new Location(move[0], move[1]);
+      float qMove = qScore(mySurroundings, moveLocation, myInfo) + gamma*bestActionScore(mySurroundings, moveLocation, myInfo);
+      //System.out.println(qMove);
+      //System.out.println(maxScore);
+      if (qMove > maxScore) {
+        maxScore = qMove;
+        moveIndex = i;
+        //System.out.println("MoveIndex: " + moveIndex);
+      }
+    }
+    //System.out.println("Kraj");
+
+
+    int[] move = moves.get(moveIndex);
+    nextLocation = new Location(myLocation.getX() + move[0], myLocation.getY() + move[1]);
+    addLocation(myLocation);
+    myLocation = nextLocation;
+    if (pacmanLocation != null && myLocation.equals(pacmanLocation)) {
+      pacmanLocation = null;
+    }
+
+    return moveIndex;
   }
 }
